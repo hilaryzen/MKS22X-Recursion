9551cdd519f122a7c05e27db027702d483a33de0
Hilary Zen <hzen00@stuy.edu>
2019-02-01 00:56:23 -0500

    Edited sqrt to add tolerance

--- Recursion.java
+++ Recursion.java
@@ -4,23 +4,23 @@ public class Recursion {
   *@param n any non-negative value you want to take the sqrt of
   *@return the approximate sqrt of n within a tolerance of 0.001%
   */
-  public static double sqrt(double n) {
-    return sqrtH(n, 1.0);
+  public static double sqrt(double n, double tolerance) {
+    return sqrtH(n, 1.0, tolerance);
   }
 
   //Square root helper function with guess as a parameter
-  public static double sqrtH(double n, double guess) {
-    if (Math.abs(n - guess * guess) / n < 0.00001) {
+  public static double sqrtH(double n, double guess, double tolerance) {
+    if (Math.abs(n - guess * guess) / n < tolerance) {
       return guess;
     } else {
       guess = (n / guess + guess) / 2;
-      return sqrtH(n, guess);
+      return sqrtH(n, guess, tolerance);
     }
   }
 
   public static void main(String[] args) {
-    System.out.println(sqrt(2));
-    System.out.println(sqrt(10));
+    System.out.println(sqrt(2, 0.00001));
+    System.out.println(sqrt(10, 0.00001));
   }
 
 }
